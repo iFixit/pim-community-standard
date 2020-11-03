e730447d6b667460a9ea70e0c39e5a7b2bd41688
Daniel Beardsley <daniel@ifixit.com>
2020-11-03 15:24:15 -0700

    iFixit Bundle: add as project bundle
    Add dependency on Guzzle for pinging iFixit api.

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,5 @@
 <?php
 
 return [
-    // Add your bundles here with the associated env.
-    // Ex:
-    // Acme\Bundle\AppBundle\AcmeAppBundle::class => ['dev' => true, 'test' => true, 'prod' => true]
+    iFixit\Akeneo\iFixitBundle\iFixitAkeneoBundle::class => ['all' => true]
 ];
