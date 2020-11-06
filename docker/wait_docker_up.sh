b41871d86b4c6732178df8c123c982858faaa50f
Daniel Beardsley <daniel@ifixit.com>
2020-11-06 00:25:39 -0700

    wait_docker_up: disable TTY so curl doesn't complain
    Without this, curl complains "The input device is not a TTY" when
    calling this outside of an interactive session.

--- docker/wait_docker_up.sh
+++ docker/wait_docker_up.sh
@@ -6,7 +6,7 @@ MAX_COUNTER=45
 COUNTER=1
 
 echo "Waiting for Elasticsearch server…"
-while ! docker-compose exec elasticsearch curl -s -k --fail "http://elasticsearch:9200/_cluster/health?wait_for_status=yellow&timeout=1s" > /dev/null; do
+while ! docker-compose exec -T elasticsearch curl -s -k --fail "http://elasticsearch:9200/_cluster/health?wait_for_status=yellow&timeout=1s" > /dev/null; do
     COUNTER=$((${COUNTER} + 1))
     if [ ${COUNTER} -gt ${MAX_COUNTER} ]; then
         echo "We have been waiting for Elasticsearch too long already; failing." >&2
