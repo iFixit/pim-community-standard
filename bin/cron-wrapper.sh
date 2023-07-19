2242d0b29ee5aa0f0844ec93b136d396e341af2c
Daniel Beardsley <daniel@ifixit.com>
2023-07-19 11:49:03 -0700

    cron-wrapper: use bash strict mode
    Note: I didn't change IFS for strict mode because it broke the way
    we were invoking docker-compose and console.
    
    And use the more idiomatic BASH_SOURCE for getting the current dir.

--- bin/cron-wrapper.sh
+++ bin/cron-wrapper.sh
@@ -6,7 +6,9 @@
 # It runs the command and writes the output (with timestamps) to a file in
 # var/logs/
 
-set -e
+# bash strict-mode
+set -euo pipefail
+
 # The entire command line is sent as a single arg
 console_command="$2" # first arg is always '-c'
 # Strip off any space-separated args to the console command
@@ -14,7 +16,7 @@ console_command_name="${console_command%% *}"
 CONSOLE="docker-compose run -u www-data --rm php php bin/console"
 LOG_FILE="var/logs/$console_command_name.log"
 
-cd "$( dirname -- "$0" )"/..
+cd "$( dirname -- "${BASH_SOURCE[0]}" )"/..
 (
    echo "Running console command: $console_command"
    $CONSOLE --no-ansi $console_command 2>&1
